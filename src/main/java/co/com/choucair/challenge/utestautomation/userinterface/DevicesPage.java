3fc374501a4f222264b3137b4157b277ebe18d2f
ddariod <dariod.dazaa09@gmail.com>
2021-08-11 06:48:38 -0500

    Change xpath call

--- src/main/java/co/com/choucair/challenge/utestautomation/userinterface/DevicesPage.java
+++ src/main/java/co/com/choucair/challenge/utestautomation/userinterface/DevicesPage.java
@@ -6,17 +6,17 @@ import org.openqa.selenium.By;
 
 public class DevicesPage extends PageObject {
     public static final Target PC_SELECTOR = Target.the("Pc reference selector")
-            .locatedBy("//*[@id=\"web-device\"]/div[1]/div[2]/div/div[1]/span");
+            .located(By.xpath("//*[@id=\"web-device\"]/div[1]/div[2]/div/div[1]/span"));
     public static final Target VERSION_SELECTOR = Target.the("Pc version selector")
-            .locatedBy("//*[@id=\"web-device\"]/div[2]/div[2]/div/div[1]/span");
+            .located(By.xpath("//*[@id=\"web-device\"]/div[2]/div[2]/div/div[1]/span"));
     public static final Target LANGUAGE_SELECTOR = Target.the("Pc language selector")
-            .locatedBy("//*[@id=\"web-device\"]/div[3]/div[2]/div/div[1]/span");
+            .located(By.xpath("//*[@id=\"web-device\"]/div[3]/div[2]/div/div[1]/span"));
     public static final Target MOBILE_SELECTOR = Target.the("Mobile reference selector")
-            .locatedBy("//*[@id=\"mobile-device\"]/div[1]/div[2]/div/div[1]/span");
+            .located(By.xpath("//*[@id=\"mobile-device\"]/div[1]/div[2]/div/div[1]/span"));
     public static final Target MODEL_SELECTOR = Target.the("Mobile model selector")
-            .locatedBy("//*[@id=\"mobile-device\"]/div[2]/div[2]/div/div[1]/span");
+            .located(By.xpath("//*[@id=\"mobile-device\"]/div[2]/div[2]/div/div[1]/span"));
     public static final Target OS_SELECTOR = Target.the("Mobile operating system selector")
-            .locatedBy("//*[@id=\"mobile-device\"]/div[3]/div[2]/div/div[1]/span");
+            .located(By.xpath("//*[@id=\"mobile-device\"]/div[3]/div[2]/div/div[1]/span"));
     public static final Target LAST_STEP_BUTTON = Target.the("Button to next last step")
             .located(By.xpath("//*[@id=\"regs_container\"]/div/div[2]/div/div[2]/div/div[2]/div/a"));
 }
--- src/main/java/co/com/choucair/challenge/utestautomation/userinterface/UtestPage.java
+++ src/main/java/co/com/choucair/challenge/utestautomation/userinterface/UtestPage.java
@@ -3,9 +3,10 @@ package co.com.choucair.challenge.utestautomation.userinterface;
 import net.serenitybdd.core.pages.PageObject;
 import net.serenitybdd.screenplay.targets.Target;
 import net.thucydides.core.annotations.DefaultUrl;
+import org.openqa.selenium.By;
 
 @DefaultUrl("https://utest.com/")
 public class UtestPage extends PageObject {
     public static final Target BECOME_BUTTON = Target.the("Become a uTester button")
-            .locatedBy("//*[@id=\"mainContent\"]/div[1]/div[2]/div/a");
+            .located(By.xpath("//*[@id=\"mainContent\"]/div[1]/div[2]/div/a"));
 }
